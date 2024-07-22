adbc6fa03e303682f6abec8d266755a04d864212
Alfahan <ali.farhan160@gmail.com>
2024-07-22 12:12:43 +0700

    update: minor update (not fix)

--- src/crypto-ts/lib/query.ts
+++ src/crypto-ts/lib/query.ts
@@ -52,115 +52,104 @@ export const validateEmail = (email: string): boolean => {
 
 export const insertWithHeap = async (
 	dt: DataSource,
-    tableName: string,
-    entity: any,
-): Promise <any> => {
-    const fieldNames: string[] = [];
-    const args: any[] = [];
-    const placeholders: string[] = [];
-    const th: TextHeap[] = [];
+	tableName: string,
+	entity: any
+): Promise<any> => {
+	const fieldNames: string[] = [];
+	const args: any[] = [];
+	const placeholders: string[] = [];
+	const th: TextHeap[] = [];
 
-    for (const key in entity) {
-        if (entity.hasOwnProperty(key)) {
-
-            const fieldName = getMetadata(entity, key, 'db');
+	for (const key in entity) {
+		if (entity.hasOwnProperty(key)) {
+			const fieldName = getMetadata(entity, key, 'db');
 			if (fieldName) {
-                fieldNames.push(fieldName);
-                const value = entity[key];
-                args.push(value);
+				fieldNames.push(fieldName);
+				let value = entity[key];
 
 				const bidxCol = getMetadata(entity, key, 'bidx_col');
-
 				if (bidxCol) {
 					fieldNames.push(bidxCol);
-
-                    // const encryptedValue = encryptWithAes('AES_256_CBC', entity[key]);
-                    // args.push(encryptedValue);
-					placeholders.push(`$${placeholders.length + 1}`);
-
-					const fieldValue = entity[key];
-
-					if (fieldValue) {
-						const txtHeapTable = getMetadata(entity, key, 'txt_heap_table');
-						const { str, heaps } = buildHeap(fieldValue, txtHeapTable);
-						th.push(...heaps);
-						args.push(str);
-					}
+					const encryptedValue = encryptWithAes('AES_256_CBC', value);
+					args.push(encryptedValue);
+					placeholders.push(`$${args.length}`);
 				}
 
-				placeholders.push(`$${placeholders.length + 1}`);
+				const txtHeapTable = getMetadata(entity, key, 'txt_heap_table');
+				if (txtHeapTable) {
+					const { str, heaps } = buildHeap(value, txtHeapTable);
+					th.push(...heaps);
+					value = str;
+				}
 
-            }
-        }
-    }
+				args.push(value);
+				placeholders.push(`$${args.length}`);
+			}
+		}
+	}
 
 	await saveToHeap(dt, th);
 
-    const query = `INSERT INTO ${tableName} (${fieldNames.join(', ')}) VALUES (${placeholders.join(', ')}) RETURNING id;`;
+	const query = `INSERT INTO ${tableName} (${fieldNames.join(', ')}) VALUES (${placeholders.join(', ')}) RETURNING id;`;
 
 	const execQuery = await dt.query(query, args);
 
-    return execQuery ;
+	return execQuery;
 };
 
-// export const updateWithHeap = (
-//     tableName: string,
-//     entity: any,
-//     id: string
-// ): { query: string } => {
-//     const fieldNames: string[] = [];
-//     const placeholders: string[] = [];
-//     const args: any[] = [];
-//     const th: TextHeap[] = [];
+export const updateWithHeap = async(
+    dt: DataSource,
+	tableName: string,
+    entity: any,
+    id: string
+): Promise<any> => {
+    const fieldNames: string[] = [];
+    const placeholders: string[] = [];
+    const args: any[] = [];
+    const th: TextHeap[] = [];
 	
-//     for (const key in entity) {
-//         if (entity.hasOwnProperty(key)) {
-			
-// 			const encrypt = getMetadata(entity, key, 'encrypt');
-// 			if(encrypt) {
-// 				const value = entity[key];
-// 				const encryptAes = encryptWithAes('AES_256_CBC', value);
-// 				args.push(`'${encryptAes}'`)
-// 			}
 
-//             const fieldName = getMetadata(entity, key, 'db');
-//             if (fieldName) {
-//                 fieldNames.push(fieldName);
-//                 const value = entity[key];
-//                 args.push(`'${value}'`);
+	for (const key in entity) {
+		if (entity.hasOwnProperty(key)) {
+			const fieldName = getMetadata(entity, key, 'db');
+			if (fieldName) {
+				fieldNames.push(fieldName);
+				let value = entity[key];
 
-//                 const bidxCol = getMetadata(entity, key, 'bidx_col');
-//                 if (bidxCol) {
-//                     fieldNames.push(bidxCol);
-//                     placeholders.push(`$${placeholders.length + 1}`);
+				const bidxCol = getMetadata(entity, key, 'bidx_col');
+				if (bidxCol) {
+					fieldNames.push(bidxCol);
+					const encryptedValue = encryptWithAes('AES_256_CBC', value);
+					args.push(encryptedValue);
+					placeholders.push(`$${args.length}`);
+				}
 
-//                     const fieldValue = entity[key];
-//                     if (fieldValue) {
-//                         const txtHeapTable = getMetadata(entity, key, 'txt_heap_table');
-//                         const { str, heaps } = buildHeap(fieldValue, txtHeapTable);
-//                         th.push(...heaps);
-//                         args.push(`'${str}'`);
-//                     }
-//                 }
-// 				placeholders.push(`$${placeholders.length + 1}`);
-//             }
-//         }
-//     }
+				const txtHeapTable = getMetadata(entity, key, 'txt_heap_table');
+				if (txtHeapTable) {
+					const { str, heaps } = buildHeap(value, txtHeapTable);
+					th.push(...heaps);
+					value = str;
+				}
 
-// 	const saveHeap = saveToHeap(th);
+				args.push(value);
+				placeholders.push(`$${args.length}`);
+			}
+		}
+	}
 
-//     let query = `UPDATE ${tableName} SET `;
-//     for (let i = 0; i < fieldNames.length; i++) {
-//         query += `${fieldNames[i]} = ${args[i]}, `;
-//     }
-//     query = query.slice(0, -2); // Remove last comma and space
-//     query += ` 
-// 		WHERE id = '${id}'; 
-// 		${saveHeap.query}
-// 	`;
+	await saveToHeap(dt, th);
 
-//     return { query };
-// };
+    let query = `UPDATE ${tableName} SET `;
+    for (let i = 0; i < fieldNames.length; i++) {
+        query += `${fieldNames[i]} = '${args[i]}'  `;
+    }
+    query = query.slice(0, -2); // Remove last comma and space
+	query += `WHERE id = '${id}';`;
+
+	const execQuery = await dt.query(query, args);
+
+    return execQuery;
+};
 
 export const buildHeap = (value: string, typeHeap: string): { str: string; heaps: TextHeap[] } => {
 	const values = split(value);
--- src/examples/test_query.ts
+++ src/examples/test_query.ts
@@ -22,41 +22,23 @@ const main = async () => {
 	await dt.initialize();
 
 	const user = new CreateUserDto();
-    user.name = 'Khairul Rahadian';
-    user.email = 'rahadian.khairul@gmail.com';
-    user.address = 'Bandung Barat';
-    user.age = 30;
-    user.password = 'securepassword';
+    // user.name = 'Reka Alamsyah';
+    // user.email = 'reka.alamsyah@gmail.com';
+    // user.address = 'Cisereuh Berung';
+    // user.age = 30;
+    // user.password = 'securepassword';
 
     const tableName = 'users';
 
-    const insertWithHeap = await CryptoTs.insertWithHeap(dt, tableName, user);
-	console.log('Insert With Heap:', insertWithHeap);
+    // const insertWithHeap = await CryptoTs.insertWithHeap(dt, tableName, user);
+	// console.log('Insert With Heap:', insertWithHeap);
 
-    // const updateWithHea = CryptoTs.updateWithHeap(tableName, entity, '123e4567-e89b-12d3-a456-426614174000');
-    // console.log('Update With Heap', updateWithHea.query);
 
-    // const textHeaps = [
-    //     { content: 'example content', type: 'example_heap', hash: 'hashed_example' }
-    // ];
-    // const { query: heapQuery, args: heapArgs } = CryptoTs.saveToHeap(textHeaps);
-    // console.log('Heap Query:', heapQuery);
-    // console.log('Heap Args:', heapArgs);
+    user.name = 'Reka Alamsyah Updated'; // Update name to a new value
+    user.email = 'reka.alamsyah.updated@gmail.com'; // Update email to a new value
 
-    // const terms = ['example', 'test'];
-    // const { query: likeQuery, args: likeArgs } = CryptoTs.buildLikeQuery('column_name', 'SELECT * FROM example_table', terms);
-    // console.log('Like Query:', likeQuery);
-    // console.log('Like Args:', likeArgs);
-
-    // const searchContents = CryptoTs.searchContents('example_heap', 'example');
-    // console.log('Search Content:', searchContents.query);
-
-    // const { query: hashExistQuery, args: hashExistArgs } = CryptoTs.isHashExist('example_heap', { hash: 'hashed_example' });
-    // console.log('Hash Exist Query:', hashExistQuery);
-    // console.log('Hash Exist Args:', hashExistArgs);
-
-    // const conditions = CryptoTs.generateSQLConditions(entity);
-    // console.log('SQL Conditions:', conditions.join(' AND '));
+    const updateWithHeap = await CryptoTs.updateWithHeap(dt, tableName, user, '3adba255-bc37-4bd1-b749-1a2315cbac87');
+    console.log('Update With Heap:', updateWithHeap);
 };
 
 main();
