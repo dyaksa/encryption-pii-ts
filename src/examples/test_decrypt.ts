ffabf4ec923415cf73c387b6fe50eaf940a7d41c
Alfahan <ali.farhan160@gmail.com>
2024-07-23 23:09:37 +0700

    feat: update README.md

--- src/examples/test_decrypt.ts
+++
@@ -1,6 +0,0 @@
-import CryptoTs from "../index";
-
-// Decrypt
-const decryptedData = CryptoTs.decryptWithAes("AES_256_CBC", '1b8253f0b4a37b46bda5b5a0401b1eb3131eba40ef283b63e1243bd8b96f2a9d');
-
-console.log('Decrypted Data:', decryptedData.toString());
--- src/examples/test_query.ts
+++ src/examples/test_query.ts
@@ -21,38 +21,12 @@ const main = async () => {
 
 	await dt.initialize();
 
-	// const user = new CreateUserDto();
-    // user.name = 'Khairul Rahadian';
-    // user.email = 'khairul.rahadian@gmail.com';
-    // user.address = 'Ujung Berung';
-    // user.age = 25;
-    // user.password = 'securepassword';
-
-    // const tableName = 'users';
-
-	// console.log(user);
-    // const insertWithHeap = await CryptoTs.insertWithHeap(dt, tableName, user);
-	// console.log('Insert With Heap:', insertWithHeap);
-
-
-	// const updateUser = new UpdateUserDto();
-    // updateUser.name = 'Reka Alamsyah sadsadas paham'; // Update name to a new value
-    // updateUser.email = 'reka.alamsyah.updateasdasdsa@gmail.com'; // Update email to a new value
-    // updateUser.address = 'Cisereuh aseeemmm';
-    // updateUser.age = 30;
-    // updateUser.password = 'securepassword';
-
-    // const updateWithHeap = await CryptoTs.updateWithHeap(dt, tableName, updateUser, '4207c94f-4f08-4793-90fa-6b5ceacadf00');
-    // console.log('Update With Heap:', updateWithHeap);
-
-	
 	const user = new User();
     user.name = encryptWithAes('AES_256_CBC','Dyaksa Rahadian');
     user.email = encryptWithAes('AES_256_CBC','dyaksa.rahadian@gmail.com');
     user.address = encryptWithAes('AES_256_CBC', 'Demak Berung');
     user.age = 25;
     user.password = 'securepassword';
-
 
     const saveToHeap = await CryptoTs.buildBlindIndex(dt, user);
 	console.log('Insert With Heap :', saveToHeap);
