6947e181652eb22a9470a4a4a46694db229110ab
dyaksa <diasnour0395@gmail.com>
2024-08-02 16:50:06 +0700

    enhancement

--- src/crypto-ts/lib/aes_encryption.ts
+++ src/crypto-ts/lib/aes_encryption.ts
@@ -4,6 +4,7 @@ import algorithms from './alg';
 import keyUtil from './key_util';
 import * as dotenv from 'dotenv';
 import { AesCipher } from './types';
+import { Console } from 'console';
 
 const DEFAULT_AUTH_TAG_LENGTH = 16;
 const SUPPORTED_AUTH_TAG_MODES = ['gcm', 'ccm', 'ocb', 'chacha20-poly1305'];
@@ -11,9 +12,9 @@ const SUPPORTED_AUTH_TAG_MODES = ['gcm', 'ccm', 'ocb', 'chacha20-poly1305'];
 dotenv.config();
 
 interface AlgorithmMeta {
-  mode: string;
-  ivLen: number;
-  expectedKeyLen: number;
+	mode: string;
+	ivLen: number;
+	expectedKeyLen: number;
 }
 
 /**
@@ -21,53 +22,53 @@ interface AlgorithmMeta {
  * @return {{mode: *, ivLen: (number), expectedKeyLen: number}}
  */
 const getMetaFromAlgorithm = (alg: string): AlgorithmMeta => {
-  const algSplited = alg.split('-');
-  if (algSplited.length < 3) {
-    throw new Error('invalid aes algorithm');
-  }
+	const algSplited = alg.split('-');
+	if (algSplited.length < 3) {
+		throw new Error('invalid aes algorithm');
+	}
 
-  const keyLenInt = parseInt(algSplited[1], 10);
-  const ivLen = algSplited[2] === 'cbc' ? 16 : 12;
-  return { expectedKeyLen: keyLenInt / 8, mode: algSplited[2], ivLen };
+	const keyLenInt = parseInt(algSplited[1], 10);
+	const ivLen = algSplited[2] === 'cbc' ? 16 : 12;
+	return { expectedKeyLen: keyLenInt / 8, mode: algSplited[2], ivLen };
 };
 
 /**
  * Shim for difficult createCipheriv method
- * 
- * @param algorithm 
- * @param key 
- * @param iv 
- * @param options 
- * @returns 
+ *
+ * @param algorithm
+ * @param key
+ * @param iv
+ * @param options
+ * @returns
  */
 const createCipherivShim = (
-  algorithm: string,
-  key: Buffer,
-  iv: Buffer,
-  options: any,
+	algorithm: string,
+	key: Buffer,
+	iv: Buffer,
+	options: any,
 ): any => {
-  const cipher = createCipheriv(algorithm, key, iv, options);
-  return cipher;
-}
+	const cipher = createCipheriv(algorithm, key, iv, options);
+	return cipher;
+};
 
 /**
  * Shim for difficult createCipheriv method
- * 
- * @param algorithm 
- * @param key 
- * @param iv 
- * @param options 
- * @returns 
+ *
+ * @param algorithm
+ * @param key
+ * @param iv
+ * @param options
+ * @returns
  */
 const createDecipherivShim = (
-  algorithm: string,
-  key: Buffer,
-  iv: Buffer,
-  options: any,
+	algorithm: string,
+	key: Buffer,
+	iv: Buffer,
+	options: any,
 ): any => {
-  const cipher = createDecipheriv(algorithm, key, iv, options);
-  return cipher;
-}
+	const cipher = createDecipheriv(algorithm, key, iv, options);
+	return cipher;
+};
 
 /**
  * @param alg {string}
@@ -78,95 +79,148 @@ const createDecipherivShim = (
 const decrypt = (alg: string, key: string, data: string | Buffer): string => {
 	// Ensure data is a valid type
 	if (typeof data !== 'object' && typeof data !== 'string') {
-	  throw new Error('Error: data param should be an object or string');
+		throw new Error('Error: data param should be an object or string');
 	}
-  
+
 	const metaAlg = getMetaFromAlgorithm(alg);
-  
+
 	// Validate key length
 	if (key.length !== metaAlg.expectedKeyLen) {
-	  throw new Error(`Invalid key length, key length should be ${metaAlg.expectedKeyLen}`);
+		throw new Error(
+			`Invalid key length, key length should be ${metaAlg.expectedKeyLen}`,
+		);
 	}
-  
+
 	const keyBuf = Buffer.from(key);
-  
-	const cipherOptions = {
-	  authTagLength: DEFAULT_AUTH_TAG_LENGTH,
-	};
-  
-	// Convert data to a buffer
-	const buf = Buffer.from(data.toString('hex'), 'hex');
-	const nonceBuf = buf.subarray(0, metaAlg.ivLen);
-  
-	// Create decipher instance
-	const decipher = createDecipherivShim(alg, keyBuf, nonceBuf, cipherOptions);
-  
-	let encryptedBuf;
-	
-	// Handle authentication tag if necessary
-	if (SUPPORTED_AUTH_TAG_MODES.includes(metaAlg.mode)) {
-	  const sFrom = buf.length - DEFAULT_AUTH_TAG_LENGTH;
-	  const authTagUtf8 = buf.subarray(sFrom, buf.length);
-	  decipher.setAuthTag(authTagUtf8);
-	  encryptedBuf = buf.subarray(metaAlg.ivLen, sFrom);
-	} else {
-	  encryptedBuf = buf.subarray(metaAlg.ivLen, buf.length);
+
+	const decodedBuffer = Buffer.from(data.toString('hex'));
+	const decodedLen = decodedBuffer.length / 2;
+	const encryptedDataOut = new Uint8Array(decodedLen);
+	let encryptedDataOutN = 0;
+
+	for (let i = 0; i < decodedBuffer.length; i += 2) {
+		encryptedDataOut[i / 2] = parseInt(
+			data.toString('hex').substr(i, 2),
+			16,
+		);
 	}
-  
-	// Perform decryption
-	let decrypted = decipher.update(encryptedBuf);
-	let remaining = decipher.final();
-  
-	// Concatenate decrypted buffers
-	const resultBuffer = Buffer.concat([decrypted, remaining], decrypted.length + remaining.length);
-	return resultBuffer.toString();
-  };
-  
+	encryptedDataOutN = encryptedDataOut.length;
 
-export const decryptWithAes = (type: string, data: string | Buffer): string	 => {
-  const key = process.env.CRYPTO_AES_KEY;
-  let decryptValue: string = null;
-  switch (type) {
-    case 'AES_128_CBC':
-		decryptValue = decrypt(algorithms.AES_128_CBC, key, data);
-		break;
-    case 'AES_192_CBC':
-		decryptValue = decrypt(algorithms.AES_192_CBC, key, data);
-		break;
-	case 'AES_256_CBC':
-    	decryptValue = decrypt(algorithms.AES_256_CBC, key, data);
-		break;
-    case 'AES_128_GCM':
-		decryptValue = decrypt(algorithms.AES_128_GCM, key, data);
-		break;
-    case 'AES_192_GCM':
-    	decryptValue = decrypt(algorithms.AES_192_GCM, key, data);
-		break;
-    case 'AES_256_GCM':
-    	decryptValue = decrypt(algorithms.AES_256_GCM, key, data);
-    case 'AES_128_CCM':
-        decryptValue = decrypt(algorithms.AES_128_CCM, key, data);
-    case 'AES_192_CCM':
-		decryptValue = decrypt(algorithms.AES_192_CCM, key, data);
-		break;
-    case 'AES_256_CCM':
-        decryptValue = decrypt(algorithms.AES_256_CCM, key, data);
-		break;
-    case 'AES_128_OCB':
-    	decryptValue = decrypt(algorithms.AES_128_OCB, key, data);
-		break;
-    case 'AES_192_OCB':
-		decryptValue = decrypt(algorithms.AES_192_OCB, key, data);
-		break;
-    case 'AES_256_OCB':
-		decryptValue = decrypt(algorithms.AES_256_OCB, key, data);
-		break;
-    default:
-      	throw new Error('Unsupported decryption type');
-  }
+	if (encryptedDataOut.length < 16) {
+		throw new Error('Invalid encrypted data');
+	}
 
-  return decryptValue
+	const cipherDataBytes = encryptedDataOut.slice(16, encryptedDataOutN);
 
+	if (cipherDataBytes.length % 16 !== 0) {
+		throw new Error('Invalid encrypted data');
+	}
+
+	const nonceBytes = encryptedDataOut.slice(0, 16);
+	console.log('Key:', keyBuf);
+	console.log('IV:', nonceBytes);
+	console.log('Cipher Data Bytes:', cipherDataBytes);
+
+	const decipher = createDecipheriv('aes-256-cbc', keyBuf, nonceBytes);
+
+	const decryptedData = Buffer.concat([
+		decipher.update(Buffer.from(cipherDataBytes)),
+		decipher.final(),
+	]);
+
+	console.log(decryptedData);
+
+	// const decryptedData = Buffer.concat([
+	// 	decipher.update(Buffer.from(cipherDataBytes)),
+	// 	decipher.final(),
+	// ]);
+
+	// console.log(decryptedData);
+
+	// const buff = keyUtil.pkcs5UnPadding(decryptedData);
+
+	// console.log(buff);
+
+	return '';
+
+	// const cipherOptions = {
+	// 	authTagLength: DEFAULT_AUTH_TAG_LENGTH,
+	// };
+
+	// // Convert data to a buffer
+	// const buf = Buffer.from(data.toString('hex'), 'hex');
+	// const nonceBuf = buf.subarray(0, metaAlg.ivLen);
+
+	// // Create decipher instance
+	// const decipher = createDecipherivShim(alg, keyBuf, nonceBuf, cipherOptions);
+
+	// let encryptedBuf;
+
+	// // Handle authentication tag if necessary
+	// if (SUPPORTED_AUTH_TAG_MODES.includes(metaAlg.mode)) {
+	// 	const sFrom = buf.length - DEFAULT_AUTH_TAG_LENGTH;
+	// 	const authTagUtf8 = buf.subarray(sFrom, buf.length);
+	// 	decipher.setAuthTag(authTagUtf8);
+	// 	encryptedBuf = buf.subarray(metaAlg.ivLen, sFrom);
+	// } else {
+	// 	encryptedBuf = buf.subarray(metaAlg.ivLen, buf.length);
+	// }
+
+	// // Perform decryption
+	// let decrypted = decipher.update(encryptedBuf);
+	// let remaining = decipher.final();
+
+	// // Concatenate decrypted buffers
+	// const resultBuffer = Buffer.concat(
+	// 	[decrypted, remaining],
+	// 	decrypted.length + remaining.length,
+	// );
+	// return resultBuffer.toString();
+};
+
+export const decryptWithAes = (type: string, data: string | Buffer): string => {
+	const key = 'dGtmY2hrc3Fodm5seGZ5bWRzdXphdmJr';
+	let decryptValue: string = null;
+	switch (type) {
+		case 'AES_128_CBC':
+			decryptValue = decrypt(algorithms.AES_128_CBC, key, data);
+			break;
+		case 'AES_192_CBC':
+			decryptValue = decrypt(algorithms.AES_192_CBC, key, data);
+			break;
+		case 'AES_256_CBC':
+			decryptValue = decrypt(algorithms.AES_256_CBC, key, data);
+			break;
+		case 'AES_128_GCM':
+			decryptValue = decrypt(algorithms.AES_128_GCM, key, data);
+			break;
+		case 'AES_192_GCM':
+			decryptValue = decrypt(algorithms.AES_192_GCM, key, data);
+			break;
+		case 'AES_256_GCM':
+			decryptValue = decrypt(algorithms.AES_256_GCM, key, data);
+		case 'AES_128_CCM':
+			decryptValue = decrypt(algorithms.AES_128_CCM, key, data);
+		case 'AES_192_CCM':
+			decryptValue = decrypt(algorithms.AES_192_CCM, key, data);
+			break;
+		case 'AES_256_CCM':
+			decryptValue = decrypt(algorithms.AES_256_CCM, key, data);
+			break;
+		case 'AES_128_OCB':
+			decryptValue = decrypt(algorithms.AES_128_OCB, key, data);
+			break;
+		case 'AES_192_OCB':
+			decryptValue = decrypt(algorithms.AES_192_OCB, key, data);
+			break;
+		case 'AES_256_OCB':
+			decryptValue = decrypt(algorithms.AES_256_OCB, key, data);
+			break;
+		default:
+			throw new Error('Unsupported decryption type');
+	}
+
+	return decryptValue;
 };
 
 /**
@@ -178,78 +232,82 @@ export const decryptWithAes = (type: string, data: string | Buffer): string	 => 
 const encrypt = (alg: string, key: string, data: string | Buffer): Buffer => {
 	const metaAlg = getMetaFromAlgorithm(alg);
 	if (key.length !== metaAlg.expectedKeyLen) {
-		throw new Error(`invalid key length, key length should be ${metaAlg.expectedKeyLen}`);
+		throw new Error(
+			`invalid key length, key length should be ${metaAlg.expectedKeyLen}`,
+		);
 	}
 
-	const nonce = keyUtil.generateRandomIV(metaAlg.ivLen);
-	const nonceBuf = Buffer.from(nonce, 'hex');
+	const plainDataPadded = keyUtil.pkcs5Padding(Buffer.from(data));
 
-	const keyBuf = Buffer.from(key);
+	const cipherDataBytes = new Uint8Array(plainDataPadded.length + 16);
 
-	const cipherOptions = {
-		authTagLength: DEFAULT_AUTH_TAG_LENGTH,
-	};
+	keyUtil.generateRandIV(cipherDataBytes.subarray(0, 16));
 
-	const cipher = createCipherivShim(alg, keyBuf, nonceBuf, cipherOptions);
-	let encrypted = cipher.update(data.toString(), 'utf8', 'hex');
-	encrypted += cipher.final('hex');
-	// https://nodejs.org/api/crypto.html#ciphergetauthtag
-	if (SUPPORTED_AUTH_TAG_MODES.includes(metaAlg.mode)) {
-		encrypted += Buffer.from(cipher.getAuthTag().toString('hex'));
-	}
+	const keyB = Buffer.from(key);
 
-	const resultBuffer = Buffer.concat([nonceBuf, Buffer.from(encrypted, 'hex')], nonceBuf.length + Buffer.from(encrypted, 'hex').length);
-	return resultBuffer;
+	const cipher = createCipheriv(
+		'aes-256-cbc',
+		keyB,
+		cipherDataBytes.subarray(0, 16),
+	);
 
+	const encrypted = Buffer.concat([
+		cipher.update(plainDataPadded),
+		cipher.final(),
+	]);
+
+	encrypted.copy(cipherDataBytes, 16);
+
+	return encrypted;
 };
 
-export const encryptWithAes = (type: string, data: string | Buffer) : any => {
-  const key = process.env.CRYPTO_AES_KEY;
-  let encryptedValue: Buffer = null;
-  switch (type) {
-    case 'AES_128_CBC':
-      encryptedValue = encrypt(algorithms.AES_128_CBC, key, data);
-      break;
-    case 'AES_192_CBC':
-      encryptedValue = encrypt(algorithms.AES_192_CBC, key, data);
-      break;
-    case 'AES_256_CBC':
-      encryptedValue = encrypt(algorithms.AES_256_CBC, key, data);
-      break;
-    case 'AES_128_GCM':
-      encryptedValue = encrypt(algorithms.AES_128_GCM, key, data);
-      break;
-    case 'AES_192_GCM':
-      encryptedValue = encrypt(algorithms.AES_192_GCM, key, data);
-      break;
-    case 'AES_256_GCM':
-      encryptedValue = encrypt(algorithms.AES_256_GCM, key, data);
-      break;
-    case 'AES_128_CCM':
-      encryptedValue = encrypt(algorithms.AES_128_CCM, key, data);
-      break;
-    case 'AES_192_CCM':
-      encryptedValue = encrypt(algorithms.AES_192_CCM, key, data);
-      break;
-    case 'AES_256_CCM':
-      encryptedValue = encrypt(algorithms.AES_256_CCM, key, data);
-      break;
-    case 'AES_128_OCB':
-      encryptedValue = encrypt(algorithms.AES_128_OCB, key, data);
-      break;
-    case 'AES_192_OCB':
-      encryptedValue = encrypt(algorithms.AES_192_OCB, key, data);
-      break;
-    case 'AES_256_OCB':
-      encryptedValue = encrypt(algorithms.AES_256_OCB, key, data);
-      break;
-    default:
-      throw new Error('Unsupported encryption type');
-  }
+export const encryptWithAes = (type: string, data: string | Buffer): any => {
+	const key = 'dGtmY2hrc3Fodm5seGZ5bWRzdXphdmJr';
+	let encryptedValue: Buffer = null;
+	switch (type) {
+		case 'AES_128_CBC':
+			encryptedValue = encrypt(algorithms.AES_128_CBC, key, data);
+			break;
+		case 'AES_192_CBC':
+			encryptedValue = encrypt(algorithms.AES_192_CBC, key, data);
+			break;
+		case 'AES_256_CBC':
+			encryptedValue = encrypt(algorithms.AES_256_CBC, key, data);
+			break;
+		case 'AES_128_GCM':
+			encryptedValue = encrypt(algorithms.AES_128_GCM, key, data);
+			break;
+		case 'AES_192_GCM':
+			encryptedValue = encrypt(algorithms.AES_192_GCM, key, data);
+			break;
+		case 'AES_256_GCM':
+			encryptedValue = encrypt(algorithms.AES_256_GCM, key, data);
+			break;
+		case 'AES_128_CCM':
+			encryptedValue = encrypt(algorithms.AES_128_CCM, key, data);
+			break;
+		case 'AES_192_CCM':
+			encryptedValue = encrypt(algorithms.AES_192_CCM, key, data);
+			break;
+		case 'AES_256_CCM':
+			encryptedValue = encrypt(algorithms.AES_256_CCM, key, data);
+			break;
+		case 'AES_128_OCB':
+			encryptedValue = encrypt(algorithms.AES_128_OCB, key, data);
+			break;
+		case 'AES_192_OCB':
+			encryptedValue = encrypt(algorithms.AES_192_OCB, key, data);
+			break;
+		case 'AES_256_OCB':
+			encryptedValue = encrypt(algorithms.AES_256_OCB, key, data);
+			break;
+		default:
+			throw new Error('Unsupported encryption type');
+	}
 
-  let cipher = new AesCipher()
-  cipher.Value = encryptedValue;
-  cipher.To = data.toString();
+	let cipher = new AesCipher();
+	cipher.Value = encryptedValue;
+	cipher.To = data.toString();
 
-  return cipher;
+	return cipher;
 };
--- src/crypto-ts/lib/key_util.ts
+++ src/crypto-ts/lib/key_util.ts
@@ -19,8 +19,8 @@ const IV_SIZE = 12;
  * @return {string}
  */
 const generateRandomIV = (size: number = IV_SIZE): string => {
-  const buf = Buffer.alloc(size);
-  return randomFillSync(buf).toString('hex');
+	const buf = Buffer.alloc(size);
+	return randomFillSync(buf).toString('hex');
 };
 
 /**
@@ -29,19 +29,54 @@ const generateRandomIV = (size: number = IV_SIZE): string => {
  * @return {void | Error}
  */
 const checkKeyInput = (key: any): void | Error => {
-  if (key.length < MIN_CUSTOM_KEY_LEN) {
-    throw new Error(`key cannot be less than ${MIN_CUSTOM_KEY_LEN}`);
-  }
+	if (key.length < MIN_CUSTOM_KEY_LEN) {
+		throw new Error(`key cannot be less than ${MIN_CUSTOM_KEY_LEN}`);
+	}
 };
+
+function pkcs5Padding(plainText: Uint8Array): Uint8Array {
+	const blockSize = 32; // AES block size is 32 bytes
+	const padding = blockSize - (plainText.length % blockSize);
+	const padtext = new Uint8Array(padding).fill(padding);
+	const paddedText = new Uint8Array(plainText.length + padding);
+
+	paddedText.set(plainText);
+	paddedText.set(padtext, plainText.length);
+
+	return paddedText;
+}
+
+function pkcs5UnPadding(src: Buffer): Buffer {
+	const length = src.length;
+	const unpadding = src[length - 1];
+	const newLength = length - unpadding;
+
+	if (newLength < 0) {
+		throw new Error('invalid encrypted data or key');
+	}
+
+	return src.slice(0, newLength);
+}
+
+function generateRandIV(buffer: Uint8Array): void {
+	try {
+		randomFillSync(buffer);
+	} catch (err) {
+		throw new Error(`Failed to generate random IV: ${err.message}`);
+	}
+}
 
 export default {
 	checkKeyInput,
 	generateRandomIV,
+	generateRandIV,
+	pkcs5Padding,
+	pkcs5UnPadding,
 	KEY_SIZE_1KB,
-	KEY_SIZE_2KB, 
+	KEY_SIZE_2KB,
 	KEY_SIZE_4KB,
 	HMAC_MINIMUM_KEY_SIZE,
 	AES_128_KEY_SIZE,
 	AES_192_KEY_SIZE,
-	AES_256_KEY_SIZE
-}
+	AES_256_KEY_SIZE,
+};
